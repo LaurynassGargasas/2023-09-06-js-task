76006855cc82c3b433e9cc450388ab545377622c
LaurynassGargasas <laurynas.gargasas@gmail.com>
2023-09-06 15:51:45 +0300

    alltasks

--- task.js
+++ task.js
@@ -61,3 +61,105 @@ function monthsToMinutes(months) {
 }
 
 console.log(monthsToMinutes(3))
+
+
+function daysToFormat (days, format) {
+    let output = ''
+
+    if (format === 'minutes') {
+        let minutes = days * 24 * 60
+        output = `${days} days - ${minutes} minutes.`
+        return output
+    }
+
+    if (format === 'hours') {
+        let hours = days * 24
+        output = `${days} days - ${hours} hours.`
+        return output
+    }
+
+    if (format === 'weeks'){
+        let weeks = (days / 7).toFixed(2)
+        output = `${days} days - ${weeks} weeks.`
+        return output
+    }
+
+    if (format === 'months') {
+        let months = (days / 30).toFixed(2)
+        output = `${days} days - ${months} months.`
+        return output
+    }
+
+    if (format === 'years') {
+        let years = (days / 365).toFixed(2)
+        output = `${days} days - ${years} years.`
+        return output
+    }
+}
+
+console.log(daysToFormat(14, 'years'))
+
+
+
+
+function isDividable(divisor, unit) {
+
+    let remains = divisor % unit
+    let output = ''
+
+    if (remains === 0){
+        output = `${divisor} dalinasi iš ${unit}.`
+        return output
+    }
+
+    if (remains !== 0) {
+        output = `${divisor} nesidalina iš ${unit}, liekana yra ${remains}.`
+        return output
+    }
+}
+
+console.log(isDividable(8, 3))
+
+
+
+
+function isPair(word){
+
+    let remain = word.length % 2
+    let output = ''
+
+    if(remain === 0){
+        output = `Porinis raidziu skaicius`
+        return output
+    }
+
+    if(remain !== 0){
+        output = `Neporinis raidziu skaicius`
+        return output
+    }
+}
+
+console.log(isPair('lala'))
+
+
+
+function whichSymbol(text, whichOne) {
+    let number = whichOne - 1
+    let theLeter = text[number]
+
+    let output = `Teksto "${text}" ${whichOne} raidė yra "${theLeter}".`
+
+    if (number > text.length){
+        output = `Tekstas "${text}" turi ${text.length} simbolius, o jūs nurodėte grąžinti ${whichOne}.`
+        return output
+    } else if (number < 0){
+        number = whichOne + 1
+        theLeter = text[text.length + number]
+        output = `Teksto "${text}" ${whichOne} raidė yra "${theLeter}".`
+        return output
+    } else {
+        return output
+    }
+}
+
+console.log(whichSymbol('labass', -2))
